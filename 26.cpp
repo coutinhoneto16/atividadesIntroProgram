a984fe9791b6f985c5e499fcf99634f04a0076b3
Neto Coutinho <coutinhoneto16@gmail.com>
2025-03-31 11:07:46 -0300

    Resolução questão 26

--- 26.cpp
+++ 26.cpp
@@ -1,3 +1,18 @@
 /*
 Escreva um programa que solicite a idade de uma pessoa e exiba se ela é maior de idade ou não.
 */
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    int idade;
+    cin >> idade;
+    if (idade == 0){
+        printf("Digite uma idade válida!");
+    }else if(idade >= 18){
+        printf("É maior de idade!");
+    }else{
+        printf("Não é maior de idade");
+    }
+}

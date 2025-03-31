5d82e4d55ba6cb0cb508bb9af96300096d9c3da2
Neto Coutinho <coutinhoneto16@gmail.com>
2025-03-30 21:19:32 -0300

    Resolução questão 25

--- 25.cpp
+++ 25.cpp
@@ -1,3 +1,24 @@
 /*
 Escreva um programa que receba um nome completo e exiba apenas o sobrenome (último nome).
 */
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string nomeCompleto;
+    getline(cin, nomeCompleto); // Lê o nome completo com espaços
+
+    string ultimoNome = "";
+    int i = (int)nomeCompleto.length() - 1;
+
+    // Percorre o nome de trás para frente até encontrar um espaço
+    while (i >= 0 && nomeCompleto[i] != ' ') {
+        ultimoNome = nomeCompleto[i] + ultimoNome; // Constrói o último nome
+        i--;
+    }
+
+    // Exibe o último nome
+    cout << "Sobrenome: " << ultimoNome << endl;
+
+}

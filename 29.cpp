8f82728b440776ca223acfe58f9634d91283abaa
Neto Coutinho <coutinhoneto16@gmail.com>
2025-03-31 11:26:16 -0300

    Resolução questão 29

--- 29.cpp
+++ 29.cpp
@@ -2,3 +2,14 @@
 Escreva um programa que solicite ao usuário um número e exiba a tabuada desse número,
 utilizando um laço de repetição.
 */
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    int numero;
+    cin >> numero;
+    for (int i=0; i <= 10; i++){
+        printf("%d x %d = %d\n",numero, i, numero * i );
+    }
+}

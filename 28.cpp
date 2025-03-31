9fceb7c33d5094c57b750d0ea97fca81c21ca189
Neto Coutinho <coutinhoneto16@gmail.com>
2025-03-31 11:20:09 -0300

    Resolução questão 28

--- 28.cpp
+++ 28.cpp
@@ -1,3 +1,22 @@
 /*
 Escreva um programa que solicite três números e exiba o maior deles.
 */
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    double numero1,numero2,numero3;
+    cin >> numero1 >> numero2 >> numero3;
+    double maior = numero1;
+    
+
+    if (numero2 > maior){
+        maior = numero2;
+        if (numero3 > maior){
+            maior = numero3;
+        }
+        }
+    printf("O maior número é %.2f\n", maior);
+    
+}

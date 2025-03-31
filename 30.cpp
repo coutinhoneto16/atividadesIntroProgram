797f1e7941d67debc54cf28bed229fa4a9ea0e37
Neto Coutinho <coutinhoneto16@gmail.com>
2025-03-31 11:33:37 -0300

    Resolução da questão 30

--- 30.cpp
+++ 30.cpp
@@ -1,3 +1,15 @@
 /*
 Escreva um programa que exiba na tela a tabuada de todos os números de 1 as 10.
 */
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main(){
+    int numero;
+    for (int i = 0; i <= 10; i++){
+        printf(" ============ TABUADA DO NÚMERO %d ============== \n", i);
+        for (numero = 0; numero <= 10; numero++){
+            printf("%d x %d = %d\n", i, numero, i * numero);
+        }
+    }
+}

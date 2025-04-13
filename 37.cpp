a6a63b6639f73767093287a6e5a62beec4b1413f
Neto Coutinho <coutinhoneto16@gmail.com>
2025-04-13 16:22:25 -0300

    ex 37 e 38

--- 37.cpp
+++ 37.cpp
@@ -2,3 +2,27 @@
 Escreva um programa que receba um número base e um expoente, e então
 calcule a potência resultante utilizando um loop de repetição.
  */
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int continuar;
+    float base, expoente;
+    continuar = 1;
+
+
+    while (continuar == 1){
+
+        printf("Digite a base e em seguida o expoente\n");
+        cin >> base >> expoente;
+
+        printf("%.1f\n",pow(base,expoente));
+        printf("Digite 1 se quiser fazer as operacoes e 0 se quiser parar \n");
+        cin >> continuar;
+
+    }
+}
--- 38.cpp
+++ 38.cpp
@@ -1,3 +1,19 @@
 /*
 Escreva um programa que leia um número inteiro e informe se o número é primo ou não
  */
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int num;
+    cin >> num;
+
+    if (num %2==0){
+        printf("Primo");
+    }else{
+        printf("Nao e primo");
+    }
+}
